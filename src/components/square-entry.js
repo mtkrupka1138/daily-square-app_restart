2c53141270c03c050baa3ab1fd6b433fb54701d9
Meghan Krupka <mkrupka.hdm@gmail.com>
2022-02-07 13:59:07 -0500

    remove tooltips

--- src/components/square-entry.js
+++ src/components/square-entry.js
@@ -9,10 +9,10 @@ function SquareSingle() {
                     <form method="POST">
                         <div class="square-container">
                             <div class="square-item" id="square1">
-                                <div class="inside-square-item" data-toggle="tooltip" data-placement="top" title="for input"></div>
+                                <div class="inside-square-item"  title="for input"></div>
                             </div>
                             <div class="square-item" id="square2">
-                                <div class="dropdown show inside-square-item" data-toggle="tooltip" data-placement="top" title="for input" >
+                                <div class="dropdown show inside-square-item" title="for input" >
                                     <select class="form-select" aria-label="Default select example">
                                         <option selected></option>
                                         <option value="happy">&#128513</option>
@@ -22,7 +22,7 @@ function SquareSingle() {
                                 </div>
                             </div>
                             <div class="square-item" id="square3">
-                                <div class="dropdown show inside-square-item" data-toggle="tooltip" data-placement="top" title="for input" >
+                                <div class="dropdown show inside-square-item" title="for input" >
                                     <select class="form-select" aria-label="Default select example">
                                         <option selected></option>
                                         <option value="1">One</option>
@@ -32,7 +32,7 @@ function SquareSingle() {
                                 </div>
                             </div>
                             <div class="square-item" id="square4">
-                                <div class="dropdown show inside-square-item" data-toggle="tooltip" data-placement="top" title="for input" >
+                                <div class="dropdown show inside-square-item" title="for input" >
                                     <select class="form-select" aria-label="Default select example">
                                         <option selected></option>
                                         <option value="1">One</option>
@@ -42,50 +42,50 @@ function SquareSingle() {
                                 </div>
                             </div>
                             <div class="square-item" id="square5">
-                                <div class="inside-square-item" data-toggle="tooltip" data-placement="top" title="for input" ></div>
+                                <div class="inside-square-item" title="for input" ></div>
                             </div>
                             <div class="square-item" id="square6">
-                                <div class="inside-square-item" data-toggle="tooltip" data-placement="left" title="for input" ></div>
+                                <div class="inside-square-item" title="for input" ></div>
                             </div>
                             <div class="square-item" id="square7">
-                                <div class="inside-square-item" data-toggle="tooltip" data-placement="right" title="for input" ></div>
+                                <div class="inside-square-item" title="for input" ></div>
                             </div>
                             <div class="square-item" id="square8">
-                                <div class="inside-square-item" data-toggle="tooltip" data-placement="left" title="for input" ></div>
+                                <div class="inside-square-item" title="for input" ></div>
                             </div>
                             <div class="square-item" id="square9">
-                                <div class="inside-square-item" data-toggle="tooltip" data-placement="right" title="for input" ></div>
+                                <div class="inside-square-item" title="for input" ></div>
                             </div>
                             <div class="square-item" id="square10">
-                                <div class="inside-square-item" data-toggle="tooltip" data-placement="left" title="for input" ></div>
+                                <div class="inside-square-item" title="for input" ></div>
                             </div>
                             <div class="square-item" id="square11">
-                                <div class="inside-square-item" data-toggle="tooltip" data-placement="right" title="for input" ></div>
+                                <div class="inside-square-item" title="for input" ></div>
                             </div>
                             <div class="square-item" id="square12">
-                                <div class="inside-square-item" data-toggle="tooltip" data-placement="left" title="for input" ></div>
+                                <div class="inside-square-item" title="for input" ></div>
                             </div>
                             <div class="square-item" id="square13">
-                                <div class="inside-square-item" data-toggle="tooltip" data-placement="bottom" title="for input" ></div>
+                                <div class="inside-square-item" title="for input" ></div>
                             </div>
                             <div class="square-item" id="square14">
-                                <div class="inside-square-item" data-toggle="tooltip" data-placement="bottom" title="for input" >
+                                <div class="inside-square-item" title="for input" >
                                     <input type="textarea" placeholder="type here" />
                                 </div>
                             </div>
                             <div class="square-item" id="square15">
-                                <div class="inside-square-item" data-toggle="tooltip" data-placement="bottom" title="for input" ></div>
+                                <div class="inside-square-item" title="for input" ></div>
                             </div>
                             <div class="square-item" id="square16">
-                                <div class="inside-square-item" data-toggle="tooltip" data-placement="right" title="for input" ></div>
+                                <div class="inside-square-item" title="for input" ></div>
                             </div>
                             <div class="main-square" id="main-square">
                                 <div class="inner-square"></div>
                                 <input type="textarea" placeholder="What defined your day?" />
                             </div>
                         </div>
-                        <button>
-                            Add Square
+                        <button type="submit" className="w-full text-lg flex justify-center bg-bluesgrays-400 text-white p-4 rounded-full tracking-wide focus:outline-none focus:shadow-outline hover:bg-indigo-600 shadow-lg cursor-pointer transition ease-in duration-300">
+                            add square
                         </button>
                     </form>
                 </div>
